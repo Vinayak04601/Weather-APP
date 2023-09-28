8bf30ed0e06cb479f27a69a846fdc5e0b1c01767
Vinayak Pandey <68140840+Vinayak04601@users.noreply.github.com>
2023-09-28 18:47:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,13 +15,11 @@ def getWeather(canvas):
     pressure = json_data['main']['pressure']
     humidity = json_data['main']['humidity']
     wind = json_data['wind']['speed']
-    sunrise = time.strftime('%I:%M:%S', time.gmtime(json_data['sys']['sunrise'] - 21600))
-    sunset = time.strftime('%I:%M:%S', time.gmtime(json_data['sys']['sunset'] - 21600))
-
+   
     final_info = condition + "\n" + str(temp) + "°C"
     final_data = "\n" + "Min Temp: " + str(min_temp) + "°C" + "\n" + "Max Temp: " + str(
         max_temp) + "°C" + "\n" + "Pressure: " + str(pressure) + "\n" + "Humidity: " + str(
-        humidity) + "\n" + "Wind Speed: " + str(wind) + "\n" + "Sunrise: " + sunrise + "\n" + "Sunset: " + sunset
+        humidity) + "\n" + "Wind Speed: " + str(wind)
     label1.config(text=final_info)
     label2.config(text=final_data)
 
